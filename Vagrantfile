1b4c4fa04a69e925a4d160c801b8e30ba590cb6f
Gonzalo Muriel <gmuriel@deloitte.es>
2023-04-03 11:39:19 +0200

    Manage the provision script to install docker

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,9 @@
 Vagrant.configure("2") do |config|
-  config.vm.define "my-mini-kube" do |mini|
-    mini.vm.box = "ubuntu/focal64"
-    mini.vm.network "forwarded_port", guest:8080, host:8080, host_ip: "127.0.0.1"
-    mini.vm.provision "shell", path: "scripts/install.sh"
-    mini.vm.provider "vrtualbox" do |v|
+  config.vm.define "docker" do |docker|
+    docker.vm.box = "ubuntu/focal64"
+    docker.vm.network "forwarded_port", guest:8080, host:8080, host_ip: "127.0.0.1"
+    docker.vm.provision "shell", path: "scripts/install.sh"
+    docker.vm.provider "vrtualbox" do |v|
       v.memory = 8192
       v.cpus=2
     end
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,22 +3,42 @@
 # remove comment to enable debugging
 # set -x
 
-# Install docker
+# Set noninteractive to avoid any input from keyboard
+export DEBIAN_FRONTEND=noninteractive
 
+# create new ssh key
+[[ ! -f /home/vagrant/.ssh/mykey ]] \
+&& mkdir -p /home/vagrant/.ssh \
+&& ssh-keygen -f /home/vagrant/.ssh/mykey -N '' \
+&& chown -R vagrant:vagrant /home/vagrant/.ssh
+
+################
+# Install docker
+################
+
+# 1. Set up the repository
 apt-get update
-apt-get install \
+apt-get -y install \
 	ca-certificates \
 	curl \
 	gnupg
-
+# 2. Add ocker's official GPG
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-
+# 3. Setting up the repository
+## Created the sources.list for install
+## Ej. deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   focal stable
 echo \
-	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \   
-	tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# 4. Install Dokcer Engine, containerd, and Docker compose
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Adding privilege to Docker. On this way it's not necessary to call sudo on each docker call
+usermod -G docker vagrant
+
+#######################
